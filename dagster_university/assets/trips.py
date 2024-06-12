7bfb8dbf56c3a4bebb54d60fb593bb7530174849
Pedro Feitosa <pedrohenriqueff@gmail.com>
2024-06-12 19:37:20 -0300

    dagster config

---
+++ dagster_university/assets/trips.py
@@ -0,0 +1,72 @@
+import requests
+from . import constants
+from dagster import asset
+import duckdb
+import os
+from dagster_duckdb import DuckDBResource
+
+@asset
+def taxi_trips_file() -> None:
+    """
+      The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
+    """
+    month_to_fetch = '2023-03'
+    raw_trips = requests.get(
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    )
+
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+        output_file.write(raw_trips.content)
+
+@asset
+def taxi_zones_file() -> None:
+    path = "https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
+    raw_zones = requests.get(path)
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_zones.content)
+
+
+@asset(
+    deps=["taxi_trips_file"]
+)
+def taxi_trips(database: DuckDBResource) -> None:
+    """
+      The raw taxi trips dataset, loaded into a DuckDB database
+    """
+    sql_query = """
+        create or replace table trips as (
+          select
+            VendorID as vendor_id,
+            PULocationID as pickup_zone_id,
+            DOLocationID as dropoff_zone_id,
+            RatecodeID as rate_code_id,
+            payment_type as payment_type,
+            tpep_dropoff_datetime as dropoff_datetime,
+            tpep_pickup_datetime as pickup_datetime,
+            trip_distance as trip_distance,
+            passenger_count as passenger_count,
+            total_amount as total_amount
+          from 'data/raw/taxi_trips_2023-03.parquet'
+        );
+    """
+
+    with database.get_connection() as conn:
+        conn.execute(sql_query)
+
+@asset(
+        deps=["taxi_zones_file"]
+        )
+def taxi_zones(database: DuckDBResource) -> None:
+    sql_query = """
+       create or replace table zones as (
+       select
+           LocationID as zone_id,
+           zone,
+           borough,
+           the_geom as geometry
+        from 'data/raw/taxi_zones.csv'
+       )
+    """
+    with database.get_connection() as conn:
+        conn.execute(sql_query)
+
